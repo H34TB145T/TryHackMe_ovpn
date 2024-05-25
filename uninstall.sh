64d0281db36f7ae1f1e1cff30d114b73dfb14273
YeminthanRiki <157350470+H34TB145T@users.noreply.github.com>
2024-05-26 01:47:55 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,9 @@
-rm -rf /bin/vpn_path.config
-rm -rf /bin/thmvpn
-rm ./vpn_path.config
-
-echo "Uninstall Successfully!"
+if [ "$(whoami)" == "root" ];
+then
+        rm -rf /bin/vpn_path.config
+        rm -rf /bin/thmvpn
+        rm ./vpn_path.config
+        echo "Uninstall Successfully!"
+else
+        echo "You dont't have permission!"
+fi
